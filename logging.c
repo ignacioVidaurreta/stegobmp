d1270f69da88ffcb3dae505c4d0e85c57d401c2c
nachito <igvidaurreta@gmail.com>
2020-05-17 15:19:24 -0300

    Finish logging

--- logging.c
+++ logging.c
@@ -25,6 +25,50 @@ char* get_timestamp(){
     return timestamp;
 }
 
+char* get_steg_name(int steg_algorithm){
+    switch(steg_algorithm){
+        case LSB1:
+            return "LSB1";
+        case LSB4:
+            return "LSB4";
+        case LSBI:
+            return "LSBI";
+        default:
+            return "Unkown";
+    }
+
+}
+
+char* get_enc_name(enc_alg enc_algorithm){
+    switch(enc_algorithm){
+        case AES128:
+            return "AES128";
+        case AES192:
+            return "AES192";
+        case AES256:
+            return "AES256";
+        case DES:
+            return "DES";
+        default:
+            return "Unknown";
+    }
+}
+
+char* get_chain_alg_name(chain_mode mode){
+    switch(mode){
+        case ECB:
+            return "ECB";
+        case CFB:
+            return "CFB";
+        case OFB:
+            return "OFB";
+        case CBC:
+            return "CBC";
+        default:
+            return "Unknown";
+    }
+}
+
 void log_parameter(parameter param, struct config* config){
 
     char* timestamp = get_timestamp();
@@ -34,6 +78,25 @@ void log_parameter(parameter param, struct config* config){
             sprintf(message, "Nombre del archivo BMP a usar: %s", config->bmp_file);
             log_info(message, config);
             break;
+        case OUT:
+            sprintf(message, "Nombre del archivo que contiene el output: %s", config->out_file);
+            log_info(message, config);
+            break;
+        case STEG:
+            sprintf(message, "Nombre del algoritmo de steganografia a usar: %s",
+                    get_steg_name(config->steg_algorithm));
+            log_info(message, config);
+            break;
+        case ENC:
+            sprintf(message, "Nombre del algoritmo de encripcion a usar: %s",
+                    get_enc_name(config->enc_algorithm));
+            log_info(message, config);
+            break;
+        case CHAIN:
+            sprintf(message, "Nombre del algoritmo de encadenamiento a usar: %s",
+                    get_chain_alg_name(config->enc_algorithm));
+            log_info(message, config);
+
         default:
             log_error("Unknown parameter", config);
     }
