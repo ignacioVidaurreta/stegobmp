e0fc6c8aaa6d8fd208bdadd6631f075f807abbd7
nachito <igvidaurreta@gmail.com>
2020-05-19 11:04:08 -0300

    Tidy up master for testing

--- main.c
+++ main.c
@@ -16,13 +16,20 @@ int main(int argc, char * argv[]){
     //program_config = parse_arguments(argc, argv);
     
     //free_config(program_config);
-    //rc4();
-     information* info = bmp_to_matrix(argv[1]);
-     run_lsb4(info);
-     printf("My matrix has width %d and %d height\n", info->width, info->height);
+    information* info = bmp_to_matrix(argv[1]);
+    char * mode = "LSBI";
+
+    if(strcmp(mode, "LSBI") == 0){
+        run_lsbi(info);
+    }else if(strcmp(mode, "RC4") == 0){
+        rc4();
+    }else if(strcmp(mode, "LSB1") == 0){
+        run_lsb1(info);
+    }else if(strcmp(mode, "LSB4") == 0){
+        run_lsb4(info);
+    }
+
+
     // log_info("Programa terminado \n\n", program_config);
     //free_config(program_config);
-    //rc4();
-    
-    //run_lsbi();
 }
