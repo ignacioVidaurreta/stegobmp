28c4cda6456157ff05e8a6b6976a3c7776e7c6e5
Marina <mfuster@itba.edu.ar>
2020-05-22 20:33:21 -0300

    Include more comments on matrix to bmp

--- bmp.c
+++ bmp.c
@@ -89,23 +89,23 @@ information* bmp_to_matrix(const char* filename) {
 /* file_name: must contain .bmp extension */
 int matrix_to_bmp(information* info, char* file_name) {
     FILE* bmp_file = fopen(file_name, "w");
-    if(bmp_file == NULL) {
-        return 1; // error
+    if(bmp_file == NULL) {                                                  // Problems when opening the file...
+        return 1;                                                           // error
     }
-    unsigned int result = fwrite(info->header, HEADER_SIZE, 1, bmp_file);
-    if(result != 1) { // result != elements to write with fwrite
-        return 1; // error
+    unsigned int result = fwrite(info->header, HEADER_SIZE, 1, bmp_file);   // Writes header into file
+    if(result != 1) {                                                       // If it didn't write what it needed to write...
+        return 1;                                                           // error
     }
     unsigned int height = info->header->bmp_height;
     unsigned int width  = info->header->bmp_width;
     for(int i=0; i<height; i++) {
         for(int j=0; j<width; j++) {
-            result = fwrite(info->matrix[i][j],sizeof(pixel),1,bmp_file);
-            if(result != 1) { // result != elements to write with fwrite
-                return 1; // error
+            result = fwrite(info->matrix[i][j],sizeof(pixel), 1,bmp_file);  // Writes every pixel into file
+            if(result != 1) {                                               // If it didn't write what it needed to write...
+                return 1;                                                   // error
             }
         }
     }
     fclose(bmp_file);
-    return 0; // success
+    return 0;                                                               // success
 }
