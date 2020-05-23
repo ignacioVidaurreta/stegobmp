52e11fd66b199465724cdc712847334dbfebe73e
Marina <mfuster@itba.edu.ar>
2020-05-22 22:43:46 -0300

    First draft to concatenate and split file information

---
+++ file_analyzer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "include/file_analyzer.h"
+
+unsigned char* concatenate(file_data* data) {
+    return NULL;
+}
+
+file_data* split(unsigned char* stream) {
+    return NULL;
+}
