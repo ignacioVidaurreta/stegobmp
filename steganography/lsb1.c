e01c31b0cce956513d2680b3e000511bf52777aa
Marina <mfuster@itba.edu.ar>
2020-05-24 18:18:20 -0300

    embed stream of bytes commented

--- steganography/lsb1.c
+++ steganography/lsb1.c
@@ -8,9 +8,52 @@
 
 #define RGB_COLOR_SIZE 8
 #define BYTE 8
+#define COMPONENTS 3
 
 #define DATA_SIZE 2 // This is a magic number according to data so i don't forget to change it in two places :)
                     // This will change once we have a structure where we recieve data, its size and extention
+
+
+/* Embeds a stream of bytes in image. If the size of stream is too large, the function return */
+// static void embed(const unsigned char* data, int data_size, pixel*** image, int width, int height) {
+
+//     if((data_size * BYTE) > (width * height * COMPONENTS * BYTE)) {           // returns if data_size it's too long to insert in image
+//         return;
+//     }
+
+//     unsigned char* bits = malloc(sizeof(unsigned char)*BYTE);                 // this array of bits will be reused
+
+//     int j = 0, x = 0, y = 0;                                                  // j -> goes through bits array in circles, x -> image height, y -> image width
+//     pixel* pixel;
+//     for(int i = 0; i < data_size; i++) {                                      // we want to embed every byte on image
+
+//         if(j % BYTE == 0) {                                                   // we need to update the byte we are embedding
+//             uchar_to_byte(bits, data[i]);                                     // bits is filled with new bits
+
+//             if( j % BYTE*COMPONENTS == 0) {                                   // we need to update the pixel we are using  
+//                 pixel = image[x][y];
+//                 if(y+1 == width) {                                            // we want to reset y every time we reached a new row
+//                     x++;
+//                     y=0;
+//                 }
+//                 else {
+//                     y++;
+//                 }                             
+//             }
+//         }
+//         if(j % COMPONENTS == 0) {                                             // blue component
+//             pixel->blue = (pixel->blue & ~1) | (bits[(j % BYTE)]-'0');
+//         }
+//         else if(j % COMPONENTS == 1) {                                        // green component
+//             pixel->green = (pixel->green & ~1) | (bits[(j % BYTE)]-'0');
+//         }
+//         else {                                                                // red component
+//             pixel->red = (pixel->red & ~1) | (bits[(j % BYTE)]-'0');
+//         }
+//         j++;
+//     }
+//     free(bits);                                                               // releasing memory
+// }
 
 //TODO: error message if image can't embed the size of the data
 static void embed(const unsigned char* data, int data_size, pixel*** image, int width, int height) {
@@ -32,40 +75,8 @@ static void embed(const unsigned char* data, int data_size, pixel*** image, int 
     }
 }
 
-// static void embed_byte(unsigned char* byte, int curr_x, int curr_y, unsigned char* first_pixel_components_to_be_embeded) {
-//     int i = 0;
-//     unsigned char first_pixel_components_to_fill = *first_pixel_components_to_be_embeded;
-
-//     for(int y = curr_y ; i < BYTE && y >= 0 ; y--) {
-//         int x = 0;
-//         if (y == curr_y) {
-//             x = curr_x + 1; // first pixel to be embeded was already embeded
-//         } 
-//         for( x ; i < BYTE && x < width ; x++ ) {
-//             pixel* pixel = image[y][x];
-//             //if(i < data_size){
-//                 pixel->blue = (pixel->blue & ~1) | (data[i++]-'0');
-//                 if(i < BYTE) {
-//                     pixel->green = (pixel->green & ~1) | (data[i++]-'0');
-//                     if(i < BYTE) {
-//                         pixel->red = (pixel->red & ~1) | (data[i++]-'0');
-//                     }
-//                     else
-//                     {
-//                         *first_pixel_components_to_be_embeded = 1;
-//                     }
-//                 }
-//                 else
-//                 {
-//                     *first_pixel_components_to_be_embeded = 2;
-//                 }
-//             //}
-//         }
-//     }
-// }
-
-static unsigned char* extract(pixel*** image, int width, int height, long data_size) {
-   // int data_size = ;//TODO: get data from first 4 bytes
+static unsigned char* extract(pixel*** image, int width, int height) {
+    int data_size = DATA_SIZE *BYTE;//TODO: get data from first 4 bytes
 
     int i = 0;
     unsigned char* data = malloc(sizeof(*data)*data_size);
