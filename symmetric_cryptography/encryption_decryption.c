8ec6a1fbe30e6bc0016469c857149904ed391d6a
Marina <mfuster@itba.edu.ar>
2020-06-14 16:06:04 -0300

    run_cipher_process now uses specific enc_alg and chain_mode types

--- symmetric_cryptography/encryption_decryption.c
+++ symmetric_cryptography/encryption_decryption.c
@@ -4,10 +4,11 @@
 #include <openssl/evp.h>
 #include <openssl/ossl_typ.h>
 
-#include "../include/errors.h"
 #include "../include/cryptography.h"
 #include "../include/encryption_decryption.h"
+#include "../include/errors.h"
 #include "../include/evp_encryption_decryption.h"
+#include "../include/parser.h"
 
 #define DES "des"
 #define AES128 "aes128"
@@ -38,33 +39,17 @@
 static unsigned char* iv = (unsigned char*)"abcdabcdabcdabcdabcdabcdabcdabcd";
 
 static cipher* ciphers[][4] = { \
-    {EVP_des_cbc,     EVP_des_ecb,     EVP_des_cfb,     EVP_des_ofb},
-    {EVP_aes_128_cbc, EVP_aes_128_ecb, EVP_aes_128_cfb, EVP_aes_128_ofb},
-    {EVP_aes_192_cbc, EVP_aes_192_ecb, EVP_aes_192_cfb, EVP_aes_192_ofb},
-    {EVP_aes_256_cbc, EVP_aes_256_ecb, EVP_aes_256_cfb, EVP_aes_256_ofb}};
+    {EVP_aes_128_ecb, EVP_aes_128_cfb, EVP_aes_128_ofb, EVP_aes_128_cbc},
+    {EVP_aes_192_ecb, EVP_aes_192_cfb, EVP_aes_192_ofb, EVP_aes_192_cbc},
+    {EVP_aes_256_ecb, EVP_aes_256_cfb, EVP_aes_256_ofb, EVP_aes_256_cbc},
+    {EVP_des_ecb,     EVP_des_cfb,     EVP_des_ofb,     EVP_des_cbc}};
 
 
 /* Returns encrypted/decrypted stream depending on operation + len of corresponding stream */
-cipher_info* run_cipher_process(char* a, char* m, char* password,
+cipher_info* run_cipher_process(enc_alg algorithm, chain_mode mode, char* password,
     int operation, unsigned char* stream, int stream_len, int append_len) {    
 
     // printf("Starting cipher process\n");
-    
-    int algorithm=-1, mode=-1;
-
-    if(strcmp(DES, a) == 0)         algorithm = DES_i;
-    else if(strcmp(AES128, a) == 0) algorithm = AES128_i;
-    else if(strcmp(AES192, a) == 0) algorithm = AES192_i;
-    else if(strcmp(AES256, a) == 0) algorithm = AES256_i;
-    else 
-        return NULL;
-
-    if(strcmp(CBC, m) == 0)      mode = CBC_i;
-    else if(strcmp(ECB, m) == 0) mode = ECB_i;
-    else if(strcmp(CFB, m) == 0) mode = CFB_i;
-    else if(strcmp(OFB, m) == 0) mode = OFB_i;
-    else 
-        return NULL;
 
     // printf("Chose algorithm %d and mode %d\n",algorithm, mode);
 
